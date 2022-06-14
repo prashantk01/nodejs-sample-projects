a8b080688721129c7d25c92cc67e12fae661eddb
Prashant Kumar <kumarprince2510@gmail.com>
2022-06-14 16:55:50 +0530

    mapbox geocoding api integrated

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,10 +2,40 @@ const request = require('request');
 require('dotenv').config()
 
 
-const query = "27.4536733,78.23933";
 // Weather stack public api
+// Accept long/lat and return weather report for location.
+const query = "27.4536733,78.23933";
 const url = "http://api.weatherstack.com/current?access_key=" + process.env.WEATHERSTACK_ACCESS_KEY + "&query=" + query;
-request({ url: url , json: true}, (error, response) => {
-    const data = response.body;
-    console.log("hi there, current temprature in your city is: \n"+data.current.temperature+" and feels like: "+data.current.feelslike);
+request({ url: url, json: true }, (error, response) => {
+    if (error) {
+        console.log("Not able to connect to weather stack api")
+    }
+    else if (response.body.error) {
+        console.log("Not able to fetch data !!")
+    }
+    else {
+        const data = response.body;
+        console.log("hi there, current temprature in your city is: \n" + data.current.temperature + " and feels like: " + data.current.feelslike);
+    }
 })
+
+
+
+// print lat/long using mapbox-api 
+// The Geocoding API allows you to search for addresses and places by name or coordinates.
+const searchPlace = "Agra India";
+const geocodingUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/{"+searchPlace+"}.json?proximity=ip&types=place%2Cpostcode%2Caddress&access_token=" + process.env.YOUR_MAPBOX_ACCESS_TOKEN;
+request({ url: geocodingUrl, json: true }, (error, response) => {
+    if (error) {
+        console.log("Not able to connect to weather stack api")
+    }
+    else if (response.body.error) {
+        console.log("Not able to fetch data !!")
+    }
+    else {
+        const data = response.body;
+        console.log("data is: ", data.features[0]);
+    }
+})
+    
+    
