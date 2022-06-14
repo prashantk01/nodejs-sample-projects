814e4662adab33fcc2643b3ab8a212965d83c940
Prashant Kumar <kumarprince2510@gmail.com>
2022-06-14 15:20:14 +0530

    fetching data from api with access key

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,11 @@
 const request = require('request');
 require('dotenv').config()
+
+
 const query = "27.4536733,78.23933";
+// Weather stack public api
 const url = "http://api.weatherstack.com/current?access_key=" + process.env.WEATHERSTACK_ACCESS_KEY + "&query=" + query;
 request({ url: url , json: true}, (error, response) => {
     const data = response.body;
-    console.log("hi user, current temprature in your city is: \n"+data.current.temperature+" and feels like: "+data.current.feelslike);
+    console.log("hi there, current temprature in your city is: \n"+data.current.temperature+" and feels like: "+data.current.feelslike);
 })
