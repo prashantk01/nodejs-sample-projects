48889f44f2305135f543e072ee70d85d269dfe2d
Prashant Kumar <kumarprince2510@gmail.com>
2022-06-27 21:59:05 +0530

    add: express server is up and running on port 3000

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,3 +1,11 @@
 const express = require('express')
 
 const app = express();
+
+app.get('', (req, res) => {
+    res.send("hello express page !!")
+})
+
+app.listen(3000, () => {
+    console.log("server is up on port 3000");
+})
