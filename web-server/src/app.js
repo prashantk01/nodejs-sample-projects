d1e78b21d241ee150fc3197cacd35e27fa033078
Prashant Kumar <kumarprince2510@gmail.com>
2022-07-03 23:14:47 +0530

    add: api change

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -6,6 +6,10 @@ app.get('', (req, res) => {
     res.send("hello express page !!")
 })
 
+app.get('/test', (req, res) => {
+    res.send("server is up and running. test done");
+})
+
 app.listen(3000, () => {
     console.log("server is up on port 3000");
 })
