4459307fbc1b28653fc0b4f64afda0ac2900be43
Prashant Kumar <kumarprince2510@gmail.com>
2022-07-08 23:00:07 +0530

    express static and public dir

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,15 +1,18 @@
+const path = require('path')
 const express = require('express')
 
-const app = express();
+const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
 
-app.get('', (req, res) => {
-    res.send("hello express page !!")
-})
+app.use(express.static(publicDirectoryPath))
 
-app.get('/test', (req, res) => {
-    res.send("server is up and running. test done");
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Philadelphia'
+    })
 })
 
 app.listen(3000, () => {
-    console.log("server is up on port 3000");
+    console.log('Server is up on port 3000.')
 })
